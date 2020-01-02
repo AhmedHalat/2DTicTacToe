0ab54edae953c43e485b5a7ea499eaaf78d8c17c
Ahmed-Halat <ahmed.halat.iseehear@gmail.com>
2020-01-02 15:52:39 -0500

    remove index and allignment fixes

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-boards = [[null, null, null, null, null, null, null, null, null],
-[null, null, null, null, null, null, null, null, null],
-[null, null, null, null, null, null, null, null, null],
-[null, null, null, null, null, null, null, null, null],
-[null, null, null, null, null, null, null, null, null],
-[null, null, null, null, null, null, null, null, null],
-[null, null, null, null, null, null, null, null, null],
-[null, null, null, null, null, null, null, null, null],
-[null, null, null, null, null, null, null, null, null]]
+boards = [
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null] ]
 turn = 'X'
 board = [null, null, null, null, null, null, null, null, null]
 num = 0
@@ -14,57 +15,57 @@ playable = -1
 fill()
 
 function showBox(e) {
-    e = e || window.event;
-    e = e.target || e.srcElement;
-    if(e.innerText) return
-    num = e.id.split('-')[1]
-    if (playable != -1 && playable != num) return
-    playable = e.id.split('-')[2]
-    board = boards[num]
-    board[e.id.split('-')[2]] = turn
-    if(turn != 'X') turn = 'X'
-    else turn = "O"
-    document.getElementById('CurrentTurn').innerText = "Player "+ turn+"'s Turn"
-    fill()
-    checkStatus()
-    console.log(boards)
+  e = e || window.event;
+  e = e.target || e.srcElement;
+  if(e.innerText) return
+  num = e.id.split('-')[1]
+  if (playable != -1 && playable != num) return
+  playable = e.id.split('-')[2]
+  board = boards[num]
+  board[e.id.split('-')[2]] = turn
+  if(turn != 'X') turn = 'X'
+  else turn = "O"
+  document.getElementById('CurrentTurn').innerText = "Player "+ turn+"'s Turn"
+  fill()
+  checkStatus()
+  console.log(boards)
 }
 
 function fill(){
-    for (let index = 0; index < board.length; index++) 
-        document.getElementById('box-'+num+'-'+index).innerText=board[index]
+  for (let index = 0; index < board.length; index++) 
+    document.getElementById('box-'+num+'-'+index).innerText=board[index]
 }
 
 function checkStatus(){
-    check = board[4]
-    //check middle
-    if(check != null)
-        for(var i = 0; i < 4; i ++)
-        if(check == board[i] && check == board[8-i]){
-            alert('Player ' + check + " wins")
-            return
-}
-
-//check top-left
-check = board[0]
-if(check != null && (check == board[1] && check == board[2] || check == board[3] && check == board[6])){
+  check = board[4]
+  //check middle
+  if(check != null)
+    for(var i = 0; i < 4; i ++)
+      if(check == board[i] && check == board[8-i]){
+        alert('Player ' + check + " wins")
+        return
+      }
+  
+  //check top-left
+  check = board[0]
+  if(check != null && (check == board[1] && check == board[2] || check == board[3] && check == board[6])){
     alert('Player ' + check + " wins")
     return
-}
-//check bottom right
-check = board[8]
-if(check != null && (check == board[6] && check == board[7] || check == board[2] && check == board[5])){
+  }
+  //check bottom right
+  check = board[8]
+  if(check != null && (check == board[6] && check == board[7] || check == board[2] && check == board[5])){
     alert('Player ' + check + " wins")
     return
-}
-if(checkfull()){
+  }
+  if(checkfull()){
     board = [ null, null, null, null, null, null, null, null, null]
     fill()
-}
+  }
 }
 
 function checkfull(){
-    for (e of board)
-        if( e == null) return false
-    return true
+  for (e of board)
+  if( e == null) return false
+  return true
 }
